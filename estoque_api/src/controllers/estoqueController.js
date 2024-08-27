b5dd08a22bc6595a679b0a994e1bcd043f7a61ed
Vithor <vithorfjm@outlook.com>
2024-08-27 10:24:07 -0300

    CRUD completo

--- estoque_api/src/controllers/estoqueController.js
+++ estoque_api/src/controllers/estoqueController.js
@@ -11,15 +11,48 @@ function postCadastrarProduto(req, res) {
         quantidade: req.body.quantidade
     }
     Produto.create(produto).then(() => {
-        res.send(produto);
+        res.send(true);
     }).catch((err) => {
         console.log(err);
         res.send(false);
     });
-    
+}
+
+async function getProdutoById(req, res) {
+    const produto = await Produto.findByPk(req.params.id);
+    res.send(produto);
+}
+
+async function atualizarProduto(req, res) {
+    await Produto.update(
+        {quantidade: req.body.quantidade},
+        {where: {
+            id: req.body.id
+        }}
+    ).then(() => res.send(true))
+    .catch((err) => {
+        console.log(err);
+        res.send(false)
+    });
+}
+
+async function excluirProduto(req, res) {
+    await Produto.destroy({
+        where: {
+            id: req.body.id
+        }
+    }).then(() => {
+        res.send(true);
+    }).catch((err) => {
+        console.log(err);
+        res.send(false);
+    })
 }
 
 module.exports = {
     getProdutos,
-    postCadastrarProduto
+    postCadastrarProduto,
+    getProdutoById,
+    atualizarProduto,
+    excluirProduto
 }
